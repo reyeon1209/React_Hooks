ebbcdddc11ec734259217a0d300558a90b5b2dc8
reyeon1209 <reyeon5368@naver.com>
2020-10-23 02:07:48 +0900

    Update useConfirm

--- useConfirm/useConfirm.js
+++ useConfirm/useConfirm.js
@@ -1,13 +1,16 @@
 // 사용자가 무언가를 하기 전에/이벤트 실행 전에 확인
-export const useConfirm = (message = "", callback, rejection) => {
-    if (typeof callback !== "function") {
+export const useConfirm = (message = "", onConfirm, onCancel) => {
+    if (!onConfirm || typeof onConfirm !== "function") {
+      return;
+    }
+    if (onCancel && typeof onCancel !== "function") {
       return;
     }
     const confirmAction = () => {
       if (confirm(message)) {
-        callback();
+        onConfirm();
       } else {
-        rejection();
+        onCancel();
       }
     };
     return confirmAction;
