3a481a78759c75330643959d1b80af5079b25f24
reyeon1209 <reyeon5368@naver.com>
2020-10-23 01:46:10 +0900

    useClick

---
+++ useClick/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import useClick from "./useClick";
+
+const App = () => {
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
---
+++ useClick/useClick.js
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from "react";
+
+export const useClick = (onClick) => {
+  if (typeof onClick !== "function") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => { // ComponentDidMount : EventListener 추가
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {  // ComponentWillUnMount : EventListener 삭제
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
