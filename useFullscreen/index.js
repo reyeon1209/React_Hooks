a4d9352f971c7a4f6e2b2cfa558e4d9bfe4b6bb7
reyeon1209 <reyeon5368@naver.com>
2020-10-23 23:22:11 +0900

    useFullscreen

---
+++ useFullscreen/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import useFullscreen from "./useFullscreen";
+
+const App = () => {
+  const onFullS = (isFull) => {
+    console.log(isFull ? "We are full" : "We are small");
+  };
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <div ref={element}>
+        <img src="https://mimgnews.pstatic.net/image/609/2020/08/17/202008170808515410_1_20200817082333249.jpg?type=w540" />
+        <button onClick={exitFull}>Exit fullscreen</button>
+      </div>
+      <button onClick={triggerFull}>Make fullscreen</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
